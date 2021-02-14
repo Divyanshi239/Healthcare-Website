60a62e27512d95a4a3c1dcb111e12952ca4f24f3
Divyanshi239 <75576256+Divyanshi239@users.noreply.github.com>
2021-02-14 17:36:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ function speak()
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
 }
+width = screen.width;
+
+if(width => "992 px")
+{
+    document.getElementById("about").style.margin="0px 50px 0px 50px";
+    document.getElementById("mission").style.margin="0px 50px 0px 50px";
+    document.getElementById("diet").style.margin="0px 50px 0px 50px";
+    document.getElementById("recipes").style.margin="0px 50px 0px 50px";
+    document.getElementById("fitness").style.margin="0px 50px 0px 50px";
+    document.getElementById("habits").style.margin="0px 50px 0px 50px";
+    document.getElementById("if").style.width="1000px";
+    document.getElementById("if").style.height="500px";
+}
