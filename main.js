1191586ca78a3230a5cc2a7f295e1010a7ba795e
Divyanshi239 <75576256+Divyanshi239@users.noreply.github.com>
2021-02-16 12:28:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-function speak()
-{
-    var synth = window.speechSynthesis;
-    speak_data = document.getElementById("mi").value;
-    var utterThis = new SpeechSynthesisUtterance(speak_data);
-    synth.speak(utterThis);
-}
 width = screen.width;
 
 if(width => "992 px")
@@ -15,13 +8,6 @@ if(width => "992 px")
     document.getElementById("recipes").style.margin="0px 50px 0px 50px";
     document.getElementById("fitness").style.margin="0px 50px 0px 50px";
     document.getElementById("habits").style.margin="0px 50px 0px 50px";
-    document.getElementById("if").style.width="1000px";
-    document.getElementById("if").style.height="500px";
-    document.getElementById("dietimg").style.width="400px";
-    document.getElementById("dietimg").style.height="300px";
-    document.getElementById("dietimg2").style.width="400px";
-    document.getElementById("dietimg2").style.height="300px";
-    document.getElementById("dietimg2").hover.width="550px"
 }
 
 //if("992px" <= width)
